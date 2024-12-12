f615d99a300c605fc5bab53823e6f9ea688e4e9e
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-12-12 18:49:54 +0200

    added context to work with products

--- src/Contexts/ProductsContexts/index.tsx
+++ src/Contexts/ProductsContexts/index.tsx
@@ -1,16 +1,24 @@
 import React, { createContext, PropsWithChildren, useState } from "react";
-import { ProductsList } from "../../api/productsAPI/types";
+import { ProductItem, ProductsList } from "../../api/productsAPI/types";
 import productsAPI from "../../api/productsAPI";
 
 interface ProductsContextInitialValueType {
   products: ProductsList;
   setProducts: (value: (((prevState: ProductsList) => ProductsList) | ProductsList)) => void;
   updateProducts(): Promise<void>;
+  getProductById(id: number): Promise<void>;
+  productById: ProductItem | null;
 }
 
 
 export const ProductsContext = createContext<ProductsContextInitialValueType>(
-  { products: [], setProducts: () => {}, updateProducts(): Promise<void> {} },
+  {
+    products: [],
+    setProducts: () => {},
+    updateProducts(): Promise<void> {},
+    getProductById(id: number): Promise<void> {},
+    productById: null,
+  },
 );
 
 interface ProductsContextProviderProps extends PropsWithChildren {
@@ -18,15 +26,24 @@ interface ProductsContextProviderProps extends PropsWithChildren {
 
 const ProductsContextProvider = ({ children }: ProductsContextProviderProps) => {
   const [products, setProducts] = useState<ProductsList>([]);
+  const [productById, setProductById] = useState<ProductItem | null>(null);
 
-  const updateProducts = () => productsAPI.getProducts().then((response) => {
-    setProducts(response.data);
-  }).catch((err) => alert(err));
+  const updateProducts = () => productsAPI.getProducts()
+    .then((response) => {
+      setProducts(response.data);
+    }).catch((err) => alert(err));
+
+  const getProductById = (id: number) => productsAPI.getProductById(id)
+    .then((response) => {
+      setProductById(response.data);
+    }).catch((err) => alert(err));
 
   return <ProductsContext value={{
     products,
     setProducts,
     updateProducts,
+    getProductById,
+    productById,
   }}>{children}</ProductsContext>;
 };
 
