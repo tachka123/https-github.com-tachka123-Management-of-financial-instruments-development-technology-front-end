02a937a2b8cf2b955d66a96766a647cb7a743933
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-12-12 15:17:11 +0200

    fixed route

--- src/Routing/Router/index.tsx
+++ src/Routing/Router/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router";
 import RoutesObject from "../Routes";
+import HomePage from "../../Pages/HomePage";
 
 const Router = () => {
   return (
     <Routes>
-      <Route path={RoutesObject.home}></Route>
+      <Route path={RoutesObject.home} element={<HomePage />}>
+      </Route>
       <Route path={RoutesObject.product(":id")}></Route>
-      <Navigate to={RoutesObject.home} />
+      <Route path={"*"} element={<Navigate to={RoutesObject.home} replace={true} />} />
     </Routes>
   );
 };
