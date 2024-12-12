eed089d8c821a6ff624017a91f317b211d63f74f
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-12-12 18:49:30 +0200

    added router with pages

--- src/Routing/Router/index.tsx
+++ src/Routing/Router/index.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Navigate, Route, Routes } from "react-router";
 import RoutesObject from "../Routes";
 import HomePage from "../../Pages/HomePage";
+import ProductPreviewPage from "../../Pages/ProductPreviewPage";
+import ProductsCartPage from "../../Pages/ProductsCartPage";
 
 const Router = () => {
   return (
     <Routes>
       <Route path={RoutesObject.home} element={<HomePage />}>
       </Route>
-      <Route path={RoutesObject.product(":id")}></Route>
+      <Route path={RoutesObject.product(":id")} element={<ProductPreviewPage />}></Route>
+      <Route path={RoutesObject.shopping_cart} element={<ProductsCartPage />}></Route>
       <Route path={"*"} element={<Navigate to={RoutesObject.home} replace={true} />} />
     </Routes>
   );
