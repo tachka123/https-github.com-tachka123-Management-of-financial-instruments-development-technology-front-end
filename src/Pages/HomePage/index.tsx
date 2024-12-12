2dfb9c51a8e347ade5b339c4cabe859068ab4f9c
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-12-12 18:50:27 +0200

    home page with preview of products list

--- src/Pages/HomePage/index.tsx
+++ src/Pages/HomePage/index.tsx
@@ -1,17 +1,28 @@
 import React, { useContext, useEffect } from "react";
 import { ProductsContext } from "../../Contexts/ProductsContexts";
 import ProductsList from "../../Components/ProductsList";
+import ProductItemMinimized from "../../Components/ProductItemMinimized";
+import { useNavigate } from "react-router";
+import Routes from "../../Routing/Routes";
 
 const HomePage = () => {
   const productsContext = useContext(ProductsContext);
+  const navigate = useNavigate();
 
   useEffect(() => {
     productsContext.updateProducts();
   }, []);
+
+  const navigateToProduct = (id: number) => () => {
+    navigate(Routes.product(id));
+  };
   return (
-    <div>
-      <ProductsList></ProductsList>
-    </div>
+    <ProductsList>
+      {productsContext.products.map(product => <ProductItemMinimized
+        onClick={navigateToProduct(product.id)}
+        key={product.id}
+        product={product} />)}
+    </ProductsList>
   );
 };
 
