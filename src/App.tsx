4ba6ca4337fd78432b5f20fd8558ea687237571f
Olexander Savchuk <savchukolexanderwork@gmail.com>
2024-12-12 18:52:18 +0200

    providing stripe as context to app

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,33 @@ import { BrowserRouter } from "react-router";
 import PageWrapper from "./Components/PageWrapper";
 import SideNavigation from "./Forms/SideNavigation";
 import Router from "./Routing/Router";
-import "./App.css";
-import productsAPI from "./api/productsAPI";
 import ProductsContextProvider from "./Contexts/ProductsContexts";
 import PageContentWrapper from "./Components/PageContentWrapper";
 
+import "./App.css";
+import ProductsCardProvider from "./Contexts/ProductsCardContext";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const stripePromise = loadStripe(import.meta.env.REACT_APP_STRIPE_PUBLIC);
+
+
 const App = () => {
   return (
-    <ProductsContextProvider>
-      <BrowserRouter>
-        <PageWrapper>
-          <SideNavigation />
-          <PageContentWrapper>
-            <Router />
-          </PageContentWrapper>
-        </PageWrapper>
-      </BrowserRouter>
-    </ProductsContextProvider>
+    <Elements stripe={stripePromise}>
+      <ProductsCardProvider>
+        <ProductsContextProvider>
+          <BrowserRouter>
+            <PageWrapper>
+              <SideNavigation />
+              <PageContentWrapper>
+                <Router />
+              </PageContentWrapper>
+            </PageWrapper>
+          </BrowserRouter>
+        </ProductsContextProvider>
+      </ProductsCardProvider>
+    </Elements>
   );
 };
 
